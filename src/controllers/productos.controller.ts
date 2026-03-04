975b2fd7c52ddeae8a61ccea5d165a9f9da1b931
sara <sara.aranda.gtz@gmail.com>
2026-03-04 14:07:47 -0600

    feat: CRUD productos

--- src/controllers/productos.controller.ts
+++ src/controllers/productos.controller.ts
@@ -1,22 +1,53 @@
 import { Request, Response } from "express";
+import { ProductoService } from "../services/productos.service";
 
-export function getProductos(req: Request, res: Response) {
-    // Aquí iría la lógica para obtener los productos de la base de datos
-    res.json({ message: "Productos obtenidos correctamente" });
+export async function getProductos(req: Request, res: Response) {
+    try{
+        const productos = await ProductoService.getAllProductos();
+        res.status(200).json(productos);
+    }
+    catch(error){
+        res.status(500).json({ message: "Error al obtener los productos" });
+    }
 }
 
-export function getProductoById(req: Request, res: Response) {
-    res.json({ message: "Producto obtenido correctamente" });
+export async function getProductoById(req: Request, res: Response) {
+    try{
+        const producto = await ProductoService.getProductoById(Number(req.params.id));
+        res.status(200).json(producto);
+    }
+    catch(error){
+        res.status(500).json({ message: "Error al obtener el producto" });
+    }
 }
 
-export function createProducto(req: Request, res: Response) {
-    res.json({ message: "Producto creado correctamente" });
+export async function createProducto(req: Request, res: Response) {
+    try{
+        const producto = await ProductoService.createProducto(req.body);
+        res.status(201).json(producto);
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({ message: "Error al crear el producto" });
+    }
 }
 
-export function updateProducto(req: Request, res: Response) {
-    res.json({ message: "Producto actualizado correctamente" });
+export async function updateProducto(req: Request, res: Response) {
+    try{
+        const producto = await ProductoService.updateProducto(Number(req.params.id), req.body);
+        res.status(200).json(producto);
+    }
+    catch(error){
+        res.status(500).json({ message: "Error al actualizar el producto" });
+    }
 }
 
-export function deleteProducto(req: Request, res: Response) {
-    res.json({ message: "Producto eliminado correctamente" });
+export async function deleteProducto(req: Request, res: Response) {
+    try{
+        const producto = await ProductoService.deleteProducto(Number(req.params.id));
+        res.status(200).json({ message: "Producto eliminado correctamente" });
+    }
+    catch(error){
+        res.status(500).json({ message: "Error al eliminar el producto" });
+    }
 }
--- src/routes/productos.routes.ts
+++ src/routes/productos.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import {deleteProducto, updateProducto, createProducto, getProductoById, getProductos } from "../controllers/productos.controller";
+import { validateRequest, idParamValidator } from "../middlewares/validation";
+import { createProductoValidator, updateProductoValidator } from "../middlewares/productos.validation";
+
+
 const router = Router({mergeParams: true}); //heredamos los params de la ruta padre
 
 router.get('/', getProductos);
-router.get('/:id', getProductoById);
-router.post('/', createProducto);
-router.put('/:id', updateProducto);
-router.delete('/:id', deleteProducto);
+router.get('/:id', idParamValidator(), validateRequest, getProductoById);
+router.post('/', createProductoValidator(), validateRequest, createProducto);
+router.put('/:id', idParamValidator(), updateProductoValidator(), validateRequest, updateProducto);
+router.delete('/:id', idParamValidator(), validateRequest, deleteProducto);
 
 export default router;
